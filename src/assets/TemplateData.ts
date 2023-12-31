2d7d0a5cf60ad8d4e816a056da223d01b1e11292
Marmik Patel <38432949+marmik13@users.noreply.github.com>
2023-12-31 14:05:17 +0530

    Update TemplateData.ts

--- src/assets/TemplateData.ts
+++ src/assets/TemplateData.ts
@@ -13,60 +13,72 @@ export const templateItems = [
   },
   {
     group: "Who's the next winner?",
-    label: '5 Rs. Less In Next Buy',
-    weight: 1,
+    label: 'Better Luck Next Time',
+    weight: 2,
     order: 2
   },
   {
     group: "Who's the next winner?",
-    label: 'Better Luck Next Time',
-    weight: 3,
+    label: '5 Rs. Less In Next Buy',
+    weight: 1,
     order: 3
   },
   {
     group: "Who's the next winner?",
-    label: '1 More Chance',
+    label: 'Better Luck Next Time',
     weight: 3,
     order: 4
   },
   {
     group: "Who's the next winner?",
-    label: 'Better Luck Next Time',
-    weight: 4,
+    label: '1 More Chance',
+    weight: 3,
     order: 5
   },
   {
     group: "Who's the next winner?",
     label: 'Better Luck Next Time',
-    weight: 2,
+    weight: 4,
     order: 6
+  },
+  {
+    group: "Who's the next winner?",
+    label: 'Better Luck Next Time',
+    weight: 2,
+    order: 7
   },
   {
     group: "Who's the next winner?",
     label: '1 Bowl Extra On Next Buy',
     weight: 1,
-    order: 7
+    order: 8
   },
   {
     group: "Who's the next winner?",
     label: 'Better Luck Next Time',
     weight: 1,
-    order: 8
+    order: 9
   },
   {
     group: "Who's the next winner?",
     label: '10 Rs. Cashback',
     weight: 1,
-    order: 9
+    order: 10
   },
   {
     group: "Who's the next winner?",
     label: '1 More Chance',
     weight: 1,
-    order: 10
+    order: 11
+  },
+  {
+    group: "Who's the next winner?",
+    label: 'Better Luck Next Time',
+    weight: 1,
+    order: 12
   },
 
-  {
+    {
     group: "Prizes",
     label: 'Better Luck Next Time',
     weight: 2,
@@ -80,57 +92,69 @@ export const templateItems = [
   },
   {
     group: "Prizes",
-    label: '5 Rs. Less In Next Buy',
-    weight: 1,
+    label: 'Better Luck Next Time',
+    weight: 2,
     order: 2
   },
   {
     group: "Prizes",
-    label: 'Better Luck Next Time',
-    weight: 3,
+    label: '5 Rs. Less In Next Buy',
+    weight: 1,
     order: 3
   },
   {
     group: "Prizes",
-    label: '1 More Chance',
+    label: 'Better Luck Next Time',
     weight: 3,
     order: 4
   },
   {
     group: "Prizes",
-    label: 'Better Luck Next Time',
-    weight: 4,
+    label: '1 More Chance',
+    weight: 3,
     order: 5
   },
   {
     group: "Prizes",
     label: 'Better Luck Next Time',
-    weight: 2,
+    weight: 4,
     order: 6
+  },
+  {
+    group: "Prizes",
+    label: 'Better Luck Next Time',
+    weight: 2,
+    order: 7
   },
   {
     group: "Prizes",
     label: '1 Bowl Extra On Next Buy',
     weight: 1,
-    order: 7
+    order: 8
   },
   {
     group: "Prizes",
     label: 'Better Luck Next Time',
     weight: 1,
-    order: 8
+    order: 9
   },
   {
     group: "Prizes",
     label: '10 Rs. Cashback',
     weight: 1,
-    order: 9
+    order: 10
   },
   {
     group: "Prizes",
     label: '1 More Chance',
     weight: 1,
-    order: 10
+    order: 11
+  },
+  {
+    group: "Prizes",
+    label: 'Better Luck Next Time',
+    weight: 1,
+    order: 12
   },
 
   {
@@ -147,56 +171,68 @@ export const templateItems = [
   },
   {
     group: "晚餐吃什麼?",
-    label: '5 Rs. Less In Next Buy',
-    weight: 1,
+    label: 'Better Luck Next Time',
+    weight: 2,
     order: 2
   },
   {
     group: "晚餐吃什麼?",
-    label: 'Better Luck Next Time',
-    weight: 3,
+    label: '5 Rs. Less In Next Buy',
+    weight: 1,
     order: 3
   },
   {
     group: "晚餐吃什麼?",
-    label: '1 More Chance',
+    label: 'Better Luck Next Time',
     weight: 3,
     order: 4
   },
   {
     group: "晚餐吃什麼?",
-    label: 'Better Luck Next Time',
-    weight: 4,
+    label: '1 More Chance',
+    weight: 3,
     order: 5
   },
   {
     group: "晚餐吃什麼?",
     label: 'Better Luck Next Time',
-    weight: 2,
+    weight: 4,
     order: 6
+  },
+  {
+    group: "晚餐吃什麼?",
+    label: 'Better Luck Next Time',
+    weight: 2,
+    order: 7
   },
   {
     group: "晚餐吃什麼?",
     label: '1 Bowl Extra On Next Buy',
     weight: 1,
-    order: 7
+    order: 8
   },
   {
     group: "晚餐吃什麼?",
     label: 'Better Luck Next Time',
     weight: 1,
-    order: 8
+    order: 9
   },
   {
     group: "晚餐吃什麼?",
     label: '10 Rs. Cashback',
     weight: 1,
-    order: 9
+    order: 10
   },
   {
     group: "晚餐吃什麼?",
     label: '1 More Chance',
     weight: 1,
-    order: 10
+    order: 11
+  },
+  {
+    group: "晚餐吃什麼?",
+    label: 'Better Luck Next Time',
+    weight: 1,
+    order: 12
   },
 ]
